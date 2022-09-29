7182a68546f9821cb37314b006c1d2c6fccf1127
Shushank-panchal <106895994+Shushank-panchal@users.noreply.github.com>
2022-09-29 16:28:10 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,12 @@
+package CoreJava;
+//Parent and child class is a part of Inheritance Class
+class Inheritance 
+{
+public static void main(String[] args)
+{
+//Creating object to print the Values
+Child1 obj = new Child1();
+obj.Speak();
+obj.Dance();
+}
+}
---
+++ Parent.java
@@ -0,0 +1,10 @@
+package CoreJava;
+// Single- level Inheritance Program
+class Parent
+{
+//Default constructor
+Parent()
+{
+	System.out.println("Default Constructor : ");
+}
+}
