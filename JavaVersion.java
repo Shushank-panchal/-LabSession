ae42ecabc247cf8c7cd242cab2730d64084ede47
Shushank-panchal <106895994+Shushank-panchal@users.noreply.github.com>
2022-09-29 16:34:06 +0530

    Add files via upload

---
+++ JavaVersion.java
@@ -0,0 +1,8 @@
+//A program to find the version of java
+package CoreJava;
+class JavaVersion {
+public static final void main(String[] args) 
+{
+System.out.println("Version of java is :"+System.getProperty("java.version"));
+}
+}
