b44f966326045d1e4a2b30639bd27a4c9608e4c0
Shushank-panchal <106895994+Shushank-panchal@users.noreply.github.com>
2022-10-07 10:55:50 +0530

    Delete JavaVersion.java

--- JavaVersion.java
+++
@@ -1,8 +0,0 @@
-//A program to find the version of java
-package CoreJava;
-class JavaVersion {
-public static final void main(String[] args) 
-{
-System.out.println("Version of java is :"+System.getProperty("java.version"));
-}
-}
